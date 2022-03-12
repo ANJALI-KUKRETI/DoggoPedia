976bfc943fdb0efaaedbaacba76b28e8dd588230
Anjali <anjalikuku098@gmail.com>
2022-03-12 23:10:16 +0530

    responsive done

--- src/App.js
+++ src/App.js
@@ -5,39 +5,52 @@ import Card from "./components/Card";
 import CardHolder from "./components/CardHolder";
 import MainBanner from "./components/MainBanner";
 import Navbar from "./components/Navbar";
+import Spinner from "./components/Spinner";
 
 const api_key = "7497a9ea-637d-4955-ab84-3601a78a3fb6";
 function App() {
   const [dogsData, setDogsData] = useState([]);
+  const [loading, setIsLoading] = useState(false);
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const request = await axios.get("https://api.thedogapi.com/v1/breeds", {
         headers: {
           "x-api-key": api_key,
         },
       });
       setDogsData(request.data.slice(0, 50));
-      console.log(request.data);
+      // console.log(request.data);
+      setIsLoading(false);
       return request;
     }
     fetchData();
-  }, [dogsData]);
+  }, []);
+
+  const showSearchResultsHandler = (searchedData) => {
+    console.log(searchedData);
+    setDogsData(searchedData);
+  };
   return (
     <div className="App">
-      <Navbar />
+      <Navbar showResults={showSearchResultsHandler} />
       <MainBanner />
-      <CardHolder>
-        {dogsData.map((dog) => (
-          <Card
-            key={dog.id}
-            image={dog.image.url}
-            name={dog.name}
-            height={dog.height.metric}
-            lifeSpan={dog.life_span}
-            breedGroup={dog.breed_group}
-          />
-        ))}
-      </CardHolder>
+      {loading && <Spinner />}
+
+      {!loading && (
+        <CardHolder>
+          {dogsData.map((dog) => (
+            <Card
+              key={dog.id}
+              image={dog.image.url}
+              name={dog.name}
+              height={dog.height.metric}
+              lifeSpan={dog.life_span}
+              breedGroup={dog.breed_group}
+            />
+          ))}
+        </CardHolder>
+      )}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FaSearch } from "react-icons/fa";
 import { BsHeart } from "react-icons/bs";
+import axios from "axios";
 import "./Navbar.css";
 
-const Navbar = () => {
+const api_key = "7497a9ea-637d-4955-ab84-3601a78a3fb6";
+
+const Navbar = ({ showResults }) => {
+  const [searchedData, setSearchedData] = useState([]);
+  const passValueHandler = (event) => {
+    event.preventDefault();
+    showResults(searchedData);
+    setSearchedData("");
+    // console.log(searchedData);
+  };
+
+  const takingValueHandler = (event) => {
+    setSearchedData(event.target.value);
+  };
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(
+        `https://api.thedogapi.com/v1/breeds/search?q=${searchedData}`,
+        {
+          headers: {
+            "x-api-key": api_key,
+          },
+        }
+      );
+      // console.log(request.data);
+      return request;
+    }
+    fetchData();
+  }, [searchedData]);
   return (
     <div className="nav">
       <div className="logo">DoggoPedia</div>
       <div className="searchBar">
-        <form>
+        <form onSubmit={passValueHandler}>
           <input
             type="text"
             className="search"
             placeholder="Who do you want to find?"
+            value={searchedData}
+            onChange={takingValueHandler}
           />
           <button type="submit" className="searchBtn">
             <FaSearch /> Search
